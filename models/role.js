22ffe8c5efb1b48aaac589558691bba0d5613c69
Askold <astakhovaskold@gmail.com>
2021-07-25 15:26:57 +0300

    Модель Role

---
+++ models/role.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose') // подключаем класс Schema и функцию model() из mongoose
+
+const role = new Schema({
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = model('Role', role)
