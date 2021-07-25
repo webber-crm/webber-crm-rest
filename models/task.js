3bd9d3a226c384a96cde0a4b6c34a0ae470b1276
Askold <astakhovaskold@gmail.com>
2021-07-25 13:19:20 +0300

    Модель Task - вывод данных и обновление

--- models/task.js
+++ models/task.js
@@ -1,6 +1,11 @@
 const {Schema, model} = require('mongoose') // подключаем класс Schema и функцию model() из mongoose
 
 const task = new Schema({
+    idx: {
+        type: Number,
+        unique: true,
+        required: true
+    },
     name: {
         type: String,
         required: true
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,15 +2,17 @@ const {Router} = require('express') // аналог const express.Router = requi
 const Task = require('../models/task')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     /*
         рендерим шаблон Handlebars:
         1 параметр - название страницы (без указания расширения .hbs), которая будет подключена
         2 параметр - объект с любыми нужными нам опциями. Например, передадим мета-title
     */
+    const tasks = await Task.find()
     res.render('tasks', {
         title: 'Задачи',
-        isTasks: true
+        isTasks: true,
+        tasks
     })
 })
 
@@ -20,13 +22,47 @@ router.get('/add', (req, res) => {
     })
 })
 
+router.post('/edit', async (req, res) => {
+    const {id} = req.body // забираем id из объекта req.body в переменную
+    delete req.body.id // удаляем req.body.id, так как в MongoDB поле называется "_id", а в нашем запросе "id"
+    /*
+        findByIdAndUpdate() - метод Mongoose, ищем по _id и обновляем
+        1 параметр - id
+        2 параметр - объект update (что обновляем)
+     */
+    // await Task.findByIdAndUpdate(id, req.body)
+    const body = {
+        name: req.body.name,
+        body: req.body.body,
+        time: {
+            estimate: req.body.estimate,
+            fact: req.body.fact
+        }
+    }
+    await Task.findByIdAndUpdate(id, body)
+    res.redirect('/tasks')
+})
+
+router.get('/:id', async (req, res) => {
+    /*
+        req.params.id - получаем значение /:id
+        course - получаем объект с курсом
+     */
+    const task = await Task.findById(req.params.id)
+    res.render('edit-task', {
+        title: `Задача #${task._id}`, // устанавливаем мета-title
+        task // передаём объект курса
+    })
+})
+
 router.post('/add', async (req, res) => {
-    console.log(req.body)
+    const tasks = await Task.find()
 
     const task = new Task({
         name: req.body.name,
         body: req.body.body,
-        time: {estimate: req.body.estimate, fact: req.body.fact}
+        time: {estimate: req.body.estimate, fact: req.body.fact},
+        idx: tasks.length + 1
     })
 
     try {
