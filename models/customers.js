63b116a0dbe8a10d45269be067223fe7a9fc314e
Askold Astakhov <astahov@element-soft.com>
2021-11-27 23:36:07 +0300

    Customers Schema refactored

--- models/customers.js
+++ models/customers.js
@@ -1,16 +1,19 @@
 const { Schema, model } = require('mongoose'); // подключаем класс Schema и функцию model() из mongoose
 
-const customers = new Schema({
-    name: {
-        type: String,
-        required: true,
+const customers = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        projects: [String],
+        price: { type: Number },
+        service: {
+            type: Schema.Types.ObjectId,
+            ref: 'Services',
+        },
     },
-    projects: [String],
-    price: { type: Number },
-    service: {
-        type: Schema.Types.ObjectId,
-        ref: 'Services',
-    },
-});
+    { timestamps: true, id: true },
+);
 
 module.exports = model('Customers', customers);
--- routes/customers.js
+++ routes/customers.js
@@ -9,102 +9,59 @@ const auth = require('../middleware/auth');
 
 const { customersValidators } = require('../utils/validators');
 
-const func = require('./func/functions');
-
 router.get('/', auth, async (req, res) => {
-    const user = await User.findById(req.session.user._id);
-    const customers = await Customer.find({ _id: { $in: user.customers } }); // выбираем клиентов, которые привязаны к пользователю
-
-    // форматируем дату начала сотрудничества
-    customers.forEach(customer => {
-        customer.info.date = func.getFormattedDate(customer.info.dateFrom);
-    });
-
-    res.json(customers);
+    const customers = await Customer.find();
+    res.json(customers || []);
 });
 
 router.post('/', auth, customersValidators, async (req, res) => {
-    const data = req.body;
+    const { body } = req;
 
     const errors = validationResult(req); // получаем ошибки валдации (если есть)
+
+    // если переменная с ошибками не пуста
     if (!errors.isEmpty()) {
-        // если переменная с ошибками не пуста
-        req.flash('error', errors.array()[0].msg);
-        return res.status(422).redirect('/customers/add');
+        return res.status(422).json({ msg: errors.array()[0].msg });
     }
-
-    data.projects = func.getJSONDataFromString(data.projects);
-
-    if (data.dateFrom) {
-        data.info = { ...data.info, dateFrom: new Date(req.body.dateFrom) };
-        delete data.dateFrom;
-    }
-
-    if (req.files) {
-        if (req.files.img) {
-            delete data.img;
-            const path = await func.uploadUserImage(req.files.img);
-            data.img = path;
-        }
-    }
-
-    const customer = new Customer(data);
 
     try {
+        const customer = new Customer(body);
+
         const current = await customer.save(); // вызываем метод класса Task для сохранения в БД
 
-        const user = await User.findByIdAndUpdate(req.session.user._id);
-        user.customers = user.customers.concat([customer._id]);
+        const user = await User.findByIdAndUpdate(req.session.user.id);
+        user.customers = user.customers.concat([customer.id]);
         await user.save();
 
-        // делаем редирект после отправки формы
         res.json(current);
     } catch (e) {
-        console.log(e);
-        res.end();
+        throw Error(e);
     }
 });
 
 router.get('/:id', auth, async (req, res) => {
-    const customer = await Customer.findById(req.params.id).populate('service');
-    customer.projects = JSON.stringify(customer.projects);
-
+    const customer = await Customer.findById(req.params.id);
     res.json(customer);
 });
 
-router.put('/:id', auth, customersValidators, async (req, res) => {
-    const { id } = req.body; // забираем id из объекта req.body в переменную
+router.patch('/:id', auth, customersValidators, async (req, res) => {
+    const { id } = req.params; // забираем id из объекта req.params в переменную
+
+    const { body } = req;
 
     const errors = validationResult(req); // получаем ошибки валдации (если есть)
     if (!errors.isEmpty()) {
-        // если переменная с ошибками не пуста
-        req.flash('error', errors.array()[0].msg);
-        return res.status(422).redirect(`/customers/${id}`);
+        return res.status(422).json({ msg: errors.array()[0].msg });
     }
 
-    delete req.body.id; // удаляем req.body.id, так как в MongoDB поле называется "_id", а в нашем запросе "id"
-
-    const data = req.body;
-
-    data.projects = func.getJSONDataFromString(data.projects);
-    data.info = { ...data.info, dateFrom: new Date(req.body.dateFrom) };
-    delete data.dateFrom;
-
-    if (req.files) {
-        if (req.files.img) {
-            const path = await func.uploadUserImage(req.files.img);
-            data.img = path;
-        }
-    }
-
-    const current = await Customer.findByIdAndUpdate(id, data);
+    const current = await Customer.findByIdAndUpdate(id, body);
     res.json(current);
 });
 
 router.delete('/:id', auth, async (req, res) => {
-    await Customer.findByIdAndRemove(req.params.id);
+    await Customer.findByIdAndDelete(req.params.id);
 
-    const user = await User.findById(req.session.user._id);
+    const user = await User.findById(req.session.user.id);
     user.customers = user.customers.filter(c => c.toString() !== req.params.id.toString());
     await user.save();
 
