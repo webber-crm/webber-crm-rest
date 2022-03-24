9dc55c0b44f6ee369a3dac713755873c75a1a32c
Askold Astakhov <astahov@element-soft.com>
2022-03-24 20:55:18 +0300

    CRM-30 Suppress global require

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 /*
     process.env.NODE_ENV - значение, которое обычно добавляют хостинг-провайдеры
  */
-const dev = require('./config.dev');
-const prod = require('./config.prod');
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = prod;
+    // eslint-disable-next-line global-require
+    module.exports = require('./config.prod');
 } else {
-    module.exports = dev;
+    // eslint-disable-next-line global-require
+    module.exports = require('./config.dev');
 }
